7d1ba401d4692bb3de5ecfb2a6c4dc6a59cb73f8
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2023-11-29 22:30:16 -0300

    Multilanguage support

--- fundamentals/hello-world/hello.go
+++ fundamentals/hello-world/hello.go
@@ -2,15 +2,37 @@ package main
 
 import "fmt"
 
-const HelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	portuguese = "Portuguse"
+	
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	portugueseHelloPrefix = "Oi, "
+)
 
-func Hello(name string) string {
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return HelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language{
+		case spanish: 
+			prefix = spanishHelloPrefix
+		case portuguese:
+			prefix = portugueseHelloPrefix
+		default:
+			prefix = englishHelloPrefix	
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
--- fundamentals/hello-world/hello_test.go
+++ fundamentals/hello-world/hello_test.go
@@ -4,13 +4,33 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Eli")
+		got := Hello("Eli", "")
+		want := "Hello, Eli"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello to people in spanish", func(t *testing.T) {
+		got := Hello("Eli", "Spanish")
+		want := "Hola, Eli"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello to people in portuguese", func(t *testing.T) {
+		got := Hello("Eli", "Portuguese")
 		want := "Hello, Eli"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Oi, World' when an empty string is supplied and language is portuguese", func(t *testing.T){
+		got := Hello("", "Portuguese")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})	
+	t.Run("say 'Hola, World' when an empty string is supplied and language is spanish", func(t *testing.T){
+		got := Hello("", "Spanish")
+		want := "Hola, World"
 		assertCorrectMessage(t, got, want)
 	})
 }
