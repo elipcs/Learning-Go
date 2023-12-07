78fe24d23d1f673204c2447036ed3754ce13452c
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2023-12-07 10:28:41 -0300

    iteration

---
+++ iteration/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+// Recieves a character and repeat "n" times
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
