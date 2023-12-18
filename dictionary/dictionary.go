6546d1990a3ae49710e6eac4d99d258427297a02
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2023-12-18 15:47:19 -0300

    refactoring

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -4,10 +4,15 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
+	var err error
+
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		err = ErrNotFound
 	}
-	return definition, nil
+
+	return definition, err
 }
