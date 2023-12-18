07258639944e9085ef56f8487273da6d6ba0a3eb
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2023-12-18 13:24:24 -0300

    bitcoin

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -1,23 +1,18 @@
 package wallet
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestWallet(t *testing.T) {
 
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
 
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
-
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
