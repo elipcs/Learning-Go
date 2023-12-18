d7536f9e1bd14813de75dd0d4e44f3edfec4f9ed
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2023-12-18 16:05:44 -0300

    method add with errors checking

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -4,7 +4,10 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	var err error
@@ -17,6 +20,20 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, err
 }
 
-func (d Dictionary) Add(word, definition string){
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	var err error
+
+	_, status := d.Search(word)
+
+	switch status {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		err = ErrWordExists
+	default:
+		err = status
+	}
+
+	return err
 }
+
